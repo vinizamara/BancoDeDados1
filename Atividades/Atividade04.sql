32a157ab4a1e27e7d6460c28ee131d5cb028159d
Vinicius Manfrin <vinizamara2@gmail.com>
2026-07-20 19:47:33 -0300

    Realizaçã ode exercciios da aula 04

--- Atividades/Atividade04.sql
+++ Atividades/Atividade04.sql
@@ -30,4 +30,32 @@ CREATE TABLE consultas (
 );
 
 -- Exercícios de Fixação:
+-- 1) Cadastre 6 pacientes
 
+-- 2) Cadastre 10 consultas para médicos e pacientes diversos
+
+-- 3) Atualize o nome do médico 'JOÃO' para 'JOÃO DA SILVA'
+
+-- 4) Atualize a data da consulta número 3 para 15/05/2026
+
+-- 5) Exclua a primeira consulta cadastrada
+
+-- 6) Liste os nomes dos médicos e a especialidade de cada um
+
+-- 7) Liste os médicos que não possuem especialidade
+
+-- 8) Liste as consultas realizadas pelo convênio UNIMED no mês de abril
+
+-- 9) Liste os nomes dos pacientes e os convênios utilizados em suas consultas
+
+-- 10) Liste os telefones dos pacientes que nunca realizaram consultas
+
+-- 11) Liste os convênios das consultas realizadas por ortopedistas
+
+-- 12) Liste os nomes e telefones dos pacientes atendidos por pediatras ou dermatologistas em abril de 2026
+
+-- 13) Cadastre a especialidade 'NEUROLOGISTA' e atualize a especialidade de 2 médicos para essa nova especialidade
+
+-- 14) Cadastre 3 consultas para médicos neurologistas no mês de maio de 2026
+
+-- 15) Atualize as consultas realizadas por pediatras em abril de 2026 para que utilizem exclusivamente o convênio 'SUS'
